1bafa6ea9d474f4d735d860741b54803015d8e00
JuanMa Pereyra <93353072+juanMaPereyraV@users.noreply.github.com>
2022-06-21 19:16:20 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const app = express ();
+
+const path = require ('path');
+
+app.set ('port',process.env.PORT || 3000);
+
+app.listen (app.get('port'), ()=> console.log('listening on port'));
+
+app.get ('/register', (req, res) => res.sendFile(path.resolve (__dirname,"./views/register.html")));
+
+app.get ('/login', (req, res) => res.sendFile(path.resolve (__dirname,"./views/login.html")));
+
+app.use (express.static(path.resolve(__dirname, "./public")));
+
